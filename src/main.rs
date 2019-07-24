f86563294fbe5f363ce87276de20c09b8db741cf
DragonWraithus <jonathan@flat.localdomain>
2019-07-24 11:16:35 -0600

    added .gitignore

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ extern crate rocket_contrib;
 extern crate tera;
 extern crate diesel;
 
+use rocket_contrib::{ templates::Template, serve::StaticFiles };
+use rocket::request::Form;
+use rocket::http::RawStr;
+use tera::Context;
+
 fn main() {
     println!("Hello, world!");
 }
