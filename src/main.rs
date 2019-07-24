9746414faf27602fd7f11d2e82dd7d8a6b35bb16
DragonWraithus <jonathan@flat.localdomain>
2019-07-24 10:57:30 -0600

    Added dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+extern crate rocket_contrib;
+extern crate tera;
+extern crate diesel;
+
 fn main() {
     println!("Hello, world!");
 }
