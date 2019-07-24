bffe2ab7625995819456ba5c2412fb830944ccea
DragonWraithus <jonathan@flat.localdomain>
2019-07-24 13:55:53 -0600

    Server and webpages are now functional.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,34 @@ use rocket::request::Form;
 use rocket::http::RawStr;
 use tera::Context;
 
+#[get("/")]
+fn index() -> Template {
+    let mut context = Context::new();
+
+    context.insert("title", &"Welcome to the Website.");
+    context.insert("content", &"Here lies a single text entry. How to add a feed?");
+    context.insert("replace", &"__<user>__\n __<@tag>__");
+    context.insert("user_quote", &"I cannot go\n\nback\nnor\nforward\n\nIn the present\nI am stuck.");
+    
+    Template::render("index", &context)
+}
+
+#[get("/user_settings")]
+fn user_settings() -> Template {
+    Template::render("user_settings", Context::new())
+}
+
+#[get("/dead_page")]
+fn dead() -> Template {
+    let mut content = Context::new();
+
+    Template::render("dead_page", &content)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", StaticFiles::from("templates"))
+        .mount("/", routes![index, user_settings, dead])
+        .attach(Template::fairing())
+        .launch();
 }
